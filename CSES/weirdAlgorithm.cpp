fd696cc815703ecdeddd84b2e1ea9a307991c07b
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-04-13 22:46:31 +0000

    feat: implementa um algoritmo estranho

---
+++ CSES/weirdAlgorithm.cpp
@@ -0,0 +1,18 @@
+// task: https://cses.fi/problemset/task/1068
+#include <iostream>
+ 
+int main(){
+    long long n;
+    std::cin >> n;
+    
+    while (n != 1) {
+        std::cout << n << " ";
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = n * 3 + 1;
+        }
+    }
+    std::cout << 1;
+    return 0;
+}
