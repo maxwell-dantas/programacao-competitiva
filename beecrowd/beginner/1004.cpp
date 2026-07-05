0fcd7119a87b15264a08662d64c1cfe8eabe6050
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-07-05 05:44:22 -0300

    problem: Simple Product

---
+++ beecrowd/beginner/1004.cpp
@@ -0,0 +1,10 @@
+// task: https://judge.beecrowd.com/en/problems/view/1004
+
+#include <iostream>
+ 
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << "PROD = " << a * b << std::endl;
+    return 0;
+}
