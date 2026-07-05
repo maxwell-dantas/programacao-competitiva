eeeb8ba25e8ff2791be646dd4c8895260bfbc3fe
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-07-05 05:45:14 -0300

    problem: Difference

---
+++ beecrowd/beginner/1007.cpp
@@ -0,0 +1,10 @@
+// task: https://judge.beecrowd.com/en/problems/view/1007
+
+#include <iostream>
+
+int main() {
+    double a, b, c, d;
+    std::cin >> a >> b >> c >> d;
+    std::cout << "DIFERENCA = " << a * b - c * d << std::endl;
+    return 0;
+}
