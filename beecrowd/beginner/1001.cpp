fb9be682841d6ae274723425fcf8bd32cca49886
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-07-05 05:43:59 -0300

    problem: Extremely Basic

---
+++ beecrowd/beginner/1001.cpp
@@ -0,0 +1,10 @@
+// task: https://judge.beecrowd.com/en/problems/view/1001
+
+#include <iostream>
+ 
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << "X = " << a + b << std::endl;
+    return 0;
+}
