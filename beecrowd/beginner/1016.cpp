b9ec1813e794ecb347190b40f3aca3d520457472
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-07-06 00:47:32 -0300

    problem: Distance

---
+++ beecrowd/beginner/1016.cpp
@@ -0,0 +1,13 @@
+// task: https://judge.beecrowd.com/en/problems/view/1016
+
+#include <iostream>
+
+int main() {
+    int distance;
+    
+    std::cin >> distance;
+
+    std::cout << distance * 2 << " minutos"<< std::endl;
+
+    return 0;
+}
